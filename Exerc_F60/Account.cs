ada0a994782d198aaa3563972144658534d78a6c
Leonardo-Dev0101 <64702342+lleonardo1993@users.noreply.github.com>
2022-04-10 17:12:27 -0300

    Exercicios final, Properties.

---
+++ Exerc_F60/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercF_60
+{
+    internal class Account
+    {
+
+        public int Numero { get; set; }
+        public string Titular { get; private set; }
+
+        public double Saldo { get; private set; }
+
+        public Account(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Account(int numero, string titular, double saldo) : this(numero, titular)
+        {
+
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+
+        }
+
+        public override string ToString()
+        {
+            return
+                "Conta"
+                + Titular
+                + ", Titular:"
+                + Numero
+                + ", Saldo $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
