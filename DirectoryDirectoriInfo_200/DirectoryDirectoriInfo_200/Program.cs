5beb5941a11a43caab20d77f3efba6134be09db6
Leonardo <lleonardo1555@gmail.com>
2023-02-23 17:50:16 -0300

    commit

---
+++ DirectoryDirectoriInfo_200/DirectoryDirectoriInfo_200/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DirectoryDirectoriInfo_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
