664e46a0aacb1f31961937688da45fe2cb74d684
Leonardo <lleonardo1555@gmail.com>
2023-02-23 18:41:55 -0300

    listando pastas com Directory

--- DirectoryDirectoriInfo_200/DirectoryDirectoriInfo_200/Program.cs
+++ DirectoryDirectoriInfo_200/DirectoryDirectoriInfo_200/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace DirectoryDirectoriInfo_200
 {
@@ -6,8 +8,24 @@ namespace DirectoryDirectoriInfo_200
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            string path = @"c:\temp\myfolder";
+
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); //listar pastas dentro do diretorio
+                Console.WriteLine("FOLDERS: ");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
     }
 }
