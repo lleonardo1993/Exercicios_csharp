1dd46953b1fe0cddfe078499c2489d3474fa0009
Leonardo <lleonardo1555@gmail.com>
2023-02-24 11:56:00 -0300

    commit path

---
+++ Path/Path/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
