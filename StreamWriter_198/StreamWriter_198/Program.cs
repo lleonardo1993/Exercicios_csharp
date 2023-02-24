e94fb202c3131764223d0df6b01d8e122c166c5a
Leonardo <lleonardo1555@gmail.com>
2023-02-24 13:37:59 -0300

    Manipulação de arquivos com StreamWriter

---
+++ StreamWriter_198/StreamWriter_198/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriter_198
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt"; //arquivo a ser modificado
+            string targetPath = @"c:\temp\file1.txt"; // arquivo modificado
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath); //leitura do arquivo
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred.");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
