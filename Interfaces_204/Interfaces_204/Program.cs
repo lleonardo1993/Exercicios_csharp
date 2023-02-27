f3b630253571df4a042068dff7a1728cdea23e5d
Leonardo <lleonardo1555@gmail.com>
2023-02-27 17:59:15 -0300

    Solução sem interface

---
+++ Interfaces_204/Interfaces_204/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Interfaces_204.Entities;
+using Interfaces_204.Services;
+
+namespace Interfaces_204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do aluguel do veiculo. ");
+            Console.Write("Modelo do Carro: ");
+            string model = Console.ReadLine();
+            Console.Write("Data de retirada do veiculo (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Data de retorno do veiculo (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com o preço por hora: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com o preço por dia: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Fatura: ");
+
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
