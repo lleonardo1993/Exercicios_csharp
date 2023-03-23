a38ffc3760f9cbfaa1dfb6e71ca11d5525489f4e
Leonardo <lleonardo1555@gmail.com>
2023-03-23 19:01:42 -0300

    Referencia da funcao passada direto no metodo.

---
+++ Exerc_234_Func_Select/Exerc_234_Func_Select/Program.cs
@@ -0,0 +1,29 @@
+﻿using Exerc_234_Func_Select.Entities;
+
+namespace Exerc_234_Func_Select
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
