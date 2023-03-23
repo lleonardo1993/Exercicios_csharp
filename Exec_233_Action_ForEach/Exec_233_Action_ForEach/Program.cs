3810da790f8bef39f17de79c5816d76f414d36b2
Leonardo <lleonardo1555@gmail.com>
2023-03-22 21:55:18 -0300

    solução convencional, passando função void no metodo foreach.

---
+++ Exec_233_Action_ForEach/Exec_233_Action_ForEach/Program.cs
@@ -0,0 +1,31 @@
+﻿using Exec_233_Action_ForEach.Entities;
+
+namespace Exec_233_Action_ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.10));
+            list.Add(new Product("Mouse", 50.40));
+            list.Add(new Product("Tablet", 350.30));
+            list.Add(new Product("HD Case", 90.20));
+
+            list.ForEach(UpdatePrice);
+
+            foreach(Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+            Console.ReadKey();
+        }
+
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
