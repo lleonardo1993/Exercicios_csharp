56bc200f9a90faf8e3198260c0dcdc9dcdc5332e
Leonardo <lleonardo1555@gmail.com>
2023-03-22 21:28:59 -0300

    Predicate, function Lambda in line.

---
+++ Exerc_232_exemplo(RemoveAll)/Exerc_232_exemplo(RemoveAll)/Program.cs
@@ -0,0 +1,28 @@
+﻿using Exerc_232_exemplo_RemoveAll_.Entities;
+
+namespace Exerc_232_exemplo_RemoveAll_
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.10));
+            list.Add(new Product("Mouse", 50.40));
+            list.Add(new Product("Tablet", 350.30));
+            list.Add(new Product("HD Case", 90.20));
+
+
+            list.RemoveAll(p => p.Price >= 100.00);
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadKey();
+        }
+    }
+}
