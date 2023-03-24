1113a1dc7b2e9b05bdc68d8a8365d321a545b8bc
Leonardo <lleonardo1555@gmail.com>
2023-03-23 21:10:12 -0300

    Referencias e operacoes do LINQ

---
+++ Exerc_236_LINQ_Referencias/Exerc_236_LINQ_Referencias/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exerc_236_LINQ_Referencias
+{
+    // Operações LINQ
+
+    // Filtering : Where, OfType  //filtra uma colection uma fonte de dados
+
+    // Sorting : OrderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse // operações para ordenar
+
+    // Set : Distinct, // elemina repetições, Except, // diferença de conjuntos, intersect, Union
+
+    // Quantification : // All, // se todos elementos corresponde a uma condição, 
+    // Any, // se a algum elemento, Contains, // se contem algum elemento que satisfaça a condição
+
+    // Projection : Select, SelectMany
+
+    // Partition : // usado para paginação // Skip, // pula tantos elementos, 
+    // Take, // pega tantos elementos
+
+    // Join : // operação de junção. Join, GroupJoin
+
+    // Grouping : GroupBy // operação de agrupamento
+
+    // Generation : Empty, // gera uma fonte de dados vazia
+
+    // Equality : SequenceEquals, // operação de igualdade
+
+    // Element : ElementAll, First, FirsOrDefault, Last, LastOrDefault, Single, SingleOrDefault
+    // pega o primeiro elemento ou o ultimo etc..
+
+    // Conversion : AsEnumerable, AsQueryable // Conversão
+
+    // Concatenation : Concat, // concatenação
+
+    // Aggregation : Aggregation, Average, Count, LongCount, Max, Min, Sum
+    // usado para fazer soma, saber a média, contagem etc..
+}
