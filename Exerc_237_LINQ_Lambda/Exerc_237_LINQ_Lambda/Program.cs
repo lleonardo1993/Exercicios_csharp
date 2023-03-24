0a2060d77e6d44ecf08224fed63df772edc90c3c
Leonardo <lleonardo1555@gmail.com>
2023-03-23 21:47:11 -0300

    consulta com LINQ e Lamba, filtro com where

---
+++ Exerc_237_LINQ_Lambda/Exerc_237_LINQ_Lambda/Program.cs
@@ -0,0 +1,50 @@
+﻿using Exerc_237_LINQ_Lambda.Entities;
+
+namespace Exerc_237_LINQ_Lambda
+{
+    internal class Program
+    {
+
+        // função auxiliar impressão na tela foreach
+
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach(T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Eletronics", Tier = 1 };
+
+
+            List<Product> products = new List<Product>()
+            {
+
+                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
+                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
+                new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
+                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
+                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
+                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
+                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
+                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
+            };
+
+            // consulta com price menor q 900 da categoria = 1
+            var r1 = products.Where(p => p.Category.Tier == 1 &&  p.Price < 900);
+
+            
+        }
+    }
+}
